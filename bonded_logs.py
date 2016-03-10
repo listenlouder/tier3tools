4aaaf861e00e63cfdfec3638ce9917defe2956db
listenlouder <wbradford@bandwidth.com>
2016-03-10 11:10:43 -0500

    house cleaning

--- bonded_logs.py
+++ bonded_logs.py
@@ -3,12 +3,10 @@ import datetime
 from os.path import expanduser
 
 _, file  = argv
-home = expanduser('~')
 
-with open('%s/Downloads/%s' % (home, file)) as f:
+with open('%s/Downloads/%s' % (expanduser('~'), file)) as f:
     log_lines = f.read().splitlines()
 
 for line in log_lines:
     if 'mobileTransportEnabled=true' in line:
-        time = datetime.datetime.fromtimestamp(int(line.split(' ')[0])/1000)
-        print time
+        print datetime.datetime.fromtimestamp(int(line.split(' ')[0])/1000)
