0f73372a4f259c5c0c9be3bb3110096dcfa31d0f
Luke E <happyhippie1337@gmail.com>
2016-10-25 14:16:03 -0400

    Updated update_tickets.py verbage

--- update_tickets.py
+++ update_tickets.py
@@ -1,12 +1,11 @@
 from Utilities import google_sheets as gs
 from Utilities import zendesk_tools as z
-# from Utilities import slack_tools as s
+
 
 def update_tickets():
     updates = gs.Worksheet("ICCID Changes in NS (Responses)")
-    needs_update = False
-    update_list = updates.get_updates()
 
+    update_list = updates.get_updates()
     for item in update_list:
         meid = item['meid']
         field = item['field']
@@ -16,25 +15,22 @@ def update_tickets():
         update = item['update?']
 
         if mac_sn == '' and iccid != '':
-            change = iccid
+            change = iccid + "  You can now depro/repro the service line and inform the customer."
         elif iccid == '' and mac_sn != '':
             change = mac_sn
         elif iccid == '' and mac_sn == '':
-            change = 'False'
+            change = 'Please inform the customer.'
+            # ^^^^  false changed to please inform the customer
         else:
             change = 'ERROR'
             print "Invalid change. Exiting..."
             exit()
 
         if update == 'Yes':
-            needs_update = True
             z.update_comment(ticket, meid, field, change)
             print 'Updated %s' % ticket
 
     print 'Done!'
-    if needs_update:
-        pass
-        # This will send a message to slack once we can get a stupid token
-        # s.send_message('room_name', 'Tier 3', 'NetSuite updates are complete. Check your tickets for updates.')
+
 
 update_tickets()
